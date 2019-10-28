27336c7a42c8a09b07e75a7db6749e6654e8e9b6
fangcao730 <54555483+fangcao730@users.noreply.github.com>
2019-10-27 23:07:06 -0400

    Project 2 Update

--- Project_2/app.R
+++ Project_2/app.R
@@ -1,46 +1,113 @@
-setwd("/Users/a11/Documents/Github/bios611-projects-fall-2019-fangcao730/Project_2")
+#setwd("https://github.com/datasci611/bios611-projects-fall-2019-fangcao730/tree/master/Project_2")
 source("helper_function.R",local=F)
 library(ggplot2)
 library(shiny)
 library(tidyverse)
 library(tidyr)
-ui <- fluidPage( 
-  titlePanel("UMD Service Visualization"),
-  fluidRow(
-  sidebarLayout( 
-    sidebarPanel( 
-      sliderInput("year", "Year:", 
-                  min = 1990, max = 2019, 
-                  step = 1, value=c(1998,2019)
-      ),
-      selectInput("variable", label = h3("Select the variable you want to explore"), 
-                  choices = list("Food pounds" = "Foodpounds", 
-                                 "Hygiene Kits" = "HygieneKits", 
-                                 "Clothing" = "Clothing",
-                                 "Bus Tickets"="BusTickets",
-                                 "School Kits"="SchoolKits",
-                                 "Financial Assistance"="Financial",
-                                 "Diapers"="Diapers")),
-      selectInput("selection", label = h3("Select the text variable you want to explore"),
-                    choices = list("Bills Paid"="Bill",
-                                   "Notes"="Note"))
-    ),
-    mainPanel( 
-      column(12,
-             verbatimTextOutput("text"),
-             br(),
-             br(),
-             p("Plot 1: Amount of Service over time") ),
-      column(12, 
-             plotOutput("serviceplot"),
-             p("Plot 2: Number of clients served of this product over time"),
-             plotOutput("clientplot")
-      ),
-      p("Plot 3: Texts input: how are people assisted?"),
-      plotOutput("words")
-    ) 
-  )
-  )
+ui<- navbarPage("UMD service visualization",
+                #introduction/background panel
+                tabPanel("Background", 
+                  strong(p("Data Source:"),style = "font-size:25px"),
+                  p("The data comes from the Urban Ministries of Durham, which provides different services to homeless people over the years. 
+                   This interface helps visualize the amount of different services provided as well as 
+                    the number of clients it reached."), 
+                  img(src='SoupCan.jpg', align = "right",width=300, height=300),
+                  uiOutput("tab"),
+                  strong(p("How to use this interface", style = "font-size:25px")),
+                  tags$div(tags$ul(
+                    tags$li("the first of which called Service by year shows the number of
+                    people receiving this service or the amount of service given out each year by selected time range."),
+                    tags$li("The second part called Text exploration visualizes the attached assistance notes 
+                            into a word cloud"),
+                    tags$li("The third part shows the size and number of households receiving food service 
+                            over the years"),
+                    tags$li("Limitation: this interface only provides visualization for years between 1998 
+                            and 2019. The years earlier than 1998 have inconsistent data quality and sparse
+                            data points and therefore clipped."),
+                    tags$li("Here's for questions: fangcao@live.unc.edu")),  style = "font-size: 14px")
+                   
+                ),
+                #this panel shows the amount of service and number of clients by selected type of service and year range
+                tabPanel("Service by year",
+                         fluidPage( 
+                           titlePanel("Annual Service"),
+                       #slider to select year range
+                               sidebarPanel(
+                                 sliderInput("year", "Slide the buttons to select Year range you want to explore:", 
+                                             min = 1990, max = 2019, 
+                                             step = 1, value=c(1998,2019)
+                                 ),
+                                 #select variable to be explored 
+                                 selectInput("variable", label = h3("Select the variable you want to explore"), 
+                                             choices = list("Food pounds" = "Foodpounds", 
+                                                            "Hygiene Kits" = "HygieneKits", 
+                                                            "Clothing" = "Clothing",
+                                                            "Bus Tickets"="BusTickets",
+                                                            "School Kits"="SchoolKits",
+                                                            "Financial Assistance"="Financial",
+                                                            "Diapers"="Diapers"))
+                                 ),
+                               mainPanel( 
+                                 p("This tab serves to visualize the amount of selected service given out and the number of clients 
+                                   served this service over the year range you want to explore. The black dots represent the total number 
+                                   of products given out each year (for e.g. food in pounds or clothing) while the blue line is there to help
+                                     you better visualize the trend in the data overtime. 
+                                  "),
+                                 p("To obtain specific numbers: 
+                                  Hover over the black dots on the plots to see roughly the amount of total service/clients
+                                   each year displayed in the grey bar below"),
+                                        p("Plot 1: Amount of Service over time"),
+                                 #hover  returns the value of y that the mouse is over 
+                                        plotOutput("serviceplot", hover = "plot_hover"),
+                                        verbatimTextOutput("info"),
+                                        p("Plot 2: Number of clients served of this product over time"),
+                                        plotOutput("clientplot", hover = "plot2_hover"),
+                                        verbatimTextOutput("info2")
+                           )
+                         
+                )
+              ),#this tab explores the string variables in the dataset: notes and types of bills paid
+                tabPanel("Text Exploration",
+                         fluidPage(
+                           fluidRow(
+                               sidebarPanel ( p("From this drop down you can choose the text note/records to visualize
+                                                what types of services or types of bills stand out"),
+                                 selectInput("selection", label = h3("Select the text variable you want to explore"),
+                                             choices = list("Bills Paid"="Bill",
+                                                            "Notes"="Note"))
+                               )
+                             , mainPanel (
+                               p("Texts input: how are people assisted?"),
+                               plotOutput("words"),
+                               p("Sometimes when people were assisted (for example, when their bills were paid) there 
+                                 are notes or record of what types of bills were paid. Words are graphed as word cloud 
+                                 in this graph above. Key words are clothing, food and electricity bills")
+                             )
+                           )
+                         )
+                         #this tab breaks down the number of families for each select input year
+                ), tabPanel("Food service for Households",
+                            fluidPage(
+                              fluidRow(
+                                sidebarPanel(
+                                  numericInput("yearinput", "Year you want to explore:", 2019, min = 1998, max = 2019),
+                                  p("click the up and down above or enter the year you want to obtain information, 
+                                    or simply enter the year")
+                                ),
+                                mainPanel(
+                                  p("This interactive plot aims to show how many distinct clients (families) were served
+                                    in the select input year, to show how many families were reached each year and each month.
+                                   The idea of Families is defined by food provided for more than one person.
+                                    For individual usage please visit the second tab."),
+                                  p("The same family coming back for service in the same month would only be counted once, 
+                                    this plot is meant to show coverage instead of count."),
+                                  
+                                  plotOutput("hh"),
+                                  verbatimTextOutput("averagesize")
+                                )
+                              )
+                            )
+                            )
 )
 server <- function(input, output) {
   #plot no. of service distributed against year
@@ -50,6 +117,9 @@ server <- function(input, output) {
     df <- sumservice(maxyear, minyear, input$variable)
     linegraph(df,df$year,df$service)+ylab("Numer of products served")
   })
+  output$info<- renderText({
+    paste0("Amount of Service=", input$plot_hover$y)
+  })
   #plot no. of clients against year receiving the input service
   output$clientplot <- renderPlot({ 
     minyear <- input$year[1]
@@ -57,9 +127,31 @@ server <- function(input, output) {
     df <- yearclient(maxyear, minyear, input$variable)
     linegraph(df, df$year, df$client)+ylab("Number of Clients served")
   })
+  #hover values
+  output$info2 <- renderText({
+    paste0("Number Of Clients=", input$plot2_hover$y)
+  })
+  #paste UMD 's url 
+  url <- a("Urban Ministries Durham: Food and Clothing Pantries", href="http://umdurham.org/what-we-do/food-pantry.html")
+  output$tab <- renderUI({
+    tagList("Link to UMD:", url)
+  })
+  #word cloud for the string variables 
   output$words <- renderPlot({ 
    wordplot(input$selection)
   })
-  
+  #plot the number of families served each year 
+  output$hh <- renderPlot({
+    household <- hhyear(input$yearinput)
+    hhgraph(household, household$month, household$n)
+  })
+  #calculates the average size of families served each year 
+  output$averagesize <- renderText({
+    household <- hhyear(input$yearinput)
+    families <- fam(input$yearinput)
+    paste("The average number of families served in", input$yearinput, "is", mean(household$n), 
+          "and the average size of families served this year is", mean(families$FoodPersons)
+          )
+  })
 }
 shinyApp(ui = ui, server = server)  
--- Project_2/helper_function.R
+++ Project_2/helper_function.R
@@ -1,4 +1,4 @@
-setwd("/Users/a11/Documents/Github/bios611-projects-fall-2019-fangcao730/Project_2")
+#setwd("/Users/a11/Documents/GitHub/bios611-projects-fall-2019-fangcao730/Project_2")
 library(ggplot2)
 library(shiny)
 library(tidyverse)
@@ -7,7 +7,8 @@ library("tm")
 library("SnowballC")
 library("wordcloud")
 library("RColorBrewer")
-umd <-read_tsv("UMD_Services.tsv")
+umd <-read_tsv("https://raw.githubusercontent.com/datasci611/bios611-projects-fall-2019-fangcao730/master/Project_2/Data/UMD_Services.tsv")
+#umd <- read_tsv("/Users/a11/Documents/GitHub/bios611-projects-fall-2019-fangcao730/Project_2/UMD_Services.tsv")
 colnames(umd) <- c("Date", "ID", "Client File Merge", "BusTickets", "Note", "FoodPersons","Foodpounds", "Clothing", 
                    "Diapers", "SchoolKits", "HygieneKits", "Ref", "Financial", "Bill", "Payer","1","2","3")
 umd$Date <- as.Date(umd$Date, "%m/%d/%Y")
@@ -18,22 +19,23 @@ sumservice <- function(maxyear, minyear, input){
     filter(get(input)<1800)%>%drop_na()%>%group_by(year)%>%summarise(service=sum(get(input)))
   yearly
 }
-#timeclient counts number of distinct clients receiving the input service at given
+#timeclient counts number of distinct clients receiving the input service at given years of input
 yearclient <- function(maxyear, minyear, input){
   clients <- total%>% filter(year<maxyear & year>=minyear)%>%select(year, matches(input),ID)%>%drop_na()%>%
     filter(get(input)<1800)%>%drop_na()%>%group_by(year)%>%summarise(client=n())  
   clients
 }
+#shows  line graph with geom_points, easier for customers to use the hover function
 linegraph <- function(df, x, y) {
-  p <- ggplot(df, aes(x=x, y=y, group=1))+geom_point(color="RoyalBlue")+geom_smooth(se=TRUE)+
-    geom_point()+xlab("year")
+  p <- ggplot(df, aes(x=x, y=y, group=1))+geom_point(color="RoyalBlue")+geom_line(color="RoyalBlue")+geom_point()
   p
 }
-
+#wordcloud plot
 wordplot <- function(input) {
     textsb <- total%>% select(matches(input))%>%drop_na()
   docs <- Corpus(VectorSource(textsb))
   toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+  #separate to space by "/n" which shows up in the matrix sometimes
   docs <- tm_map(docs, toSpace, "\n")
   dtm <- TermDocumentMatrix(docs)
   m <- as.matrix(dtm)
@@ -45,6 +47,27 @@ wordplot <- function(input) {
                  max.words=200, random.order=FALSE, rot.per=0.35, 
                  colors=brewer.pal(8, "Dark2"))
   wd
+}
+#ask for an year input and return the table with number of distinct families and the month
+hhyear <- function(input) {
+  household <- total%>% filter(year==input)%>%
+    select(year, month, ID, Foodpounds, FoodPersons)%>%drop_na()%>%filter(FoodPersons>=2)%>%
+    group_by(month)%>%summarise(n=n_distinct(ID))
+  household
+}
+
+#ask for an integrer input and return the table with each families' IDs and the month they came in for help of that given year
+#this is for calculating average size of families etc.
+fam <- function(input) {
+  families <- total%>% filter(year==input)%>%
+    select(year, month, ID, Foodpounds, FoodPersons)%>%drop_na()%>%filter(FoodPersons>=2)%>%
+    group_by(month)
+  families
+}
+#bar chart of number of families served each year (by input) by month
+hhgraph <- function(df,x,y) {
+  ggplot(df,aes(x=x, y=y))+geom_bar(stat = "identity", fill="cornflowerblue")+xlab("Month")+
+    ylab("Number of distinct Family served food")+ggtitle("Number of households served food of selected year")
 }
 
 
